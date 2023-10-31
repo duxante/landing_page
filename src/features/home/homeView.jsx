f4bac0b21f70f3f80ef7f574e29fae1ae6457461
duxante <spomenko.dukic@gmail.com>
2023-10-31 20:42:58 +0100

    media responsive

--- src/features/home/homeView.jsx
+++ src/features/home/homeView.jsx
@@ -6,27 +6,29 @@ import Button from '../../components/button/button';
 const HomeView = () => {
   return (
     <div className="homeViewHolder">
-      <div className="leftSide">
-        <HorizontalRuleIcon
-          sx={{
-            fontSize: '60px',
-            color: '#d2b356',
-          }}
-        />
-        <h1>A free and simple landing page</h1>
-        <HorizontalRuleIcon
-          sx={{
-            fontSize: '60px',
-            color: '#d2b356',
-          }}
-        />
-        <p>
-          Namari is a free landing page you can use for your projects. It is
-          free to use for your personal and commercial projects, enjoy!
-        </p>
-        <Button buttonText="Start creating today" />
+      <div className="homeViewMiddle">
+        <div className="leftSide">
+          <HorizontalRuleIcon
+            sx={{
+              fontSize: '60px',
+              color: '#d2b356',
+            }}
+          />
+          <h1>A free and simple landing page</h1>
+          <HorizontalRuleIcon
+            sx={{
+              fontSize: '60px',
+              color: '#d2b356',
+            }}
+          />
+          <p>
+            Namari is a free landing page you can use for your projects. It is
+            free to use for your personal and commercial projects, enjoy!
+          </p>
+          <Button buttonText="Start creating today" />
+        </div>
+        <img className="heart" src={heart} alt="heart" />
       </div>
-      <img className="heart" src={heart} alt="heart" />
     </div>
   );
 };
